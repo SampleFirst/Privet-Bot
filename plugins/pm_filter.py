751e595f8b5388c081cf0811ebd0b5d2495ac97d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-07 00:18:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -149,7 +149,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             logger.info(f"{user_name} has Active status for {bot_name} with {now_status}")
             return 
         else:
-            await client.send_message(LOG_CHANNEL, script.LOG_BOT.format(a=user_id, b=user_name, c=bot_name, d=now_status, e=now_date, f=now_time, g=expiry_date, h=expiry_time)
+            await client.send_message(LOG_CHANNEL, script.LOG_BOT.format(a=user_id, b=user_name, c=bot_name, d=now_status, e=now_date, f=now_time, g=expiry_date, h=expiry_time))
             await update_verification_dot(client, user_id, bot_name, now_status)
             logger.info(f"{user_name} update status for {bot_name} with {now_status}")
             buttons = [
@@ -189,7 +189,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             logger.info(f"{user_name} has Active status for {db_name} with {now_status}")
             return 
         else:
-            await client.send_message(LOG_CHANNEL, script.LOG_BOT.format(a=user_id, b=user_name, c=db_name, d=now_status, e=now_date, f=now_time, g=expiry_date, h=expiry_time)
+            await client.send_message(LOG_CHANNEL, script.LOG_BOT.format(a=user_id, b=user_name, c=db_name, d=now_status, e=now_date, f=now_time, g=expiry_date, h=expiry_time))
             await update_verification_bd(client, user_id, bot_name, now_status)
             logger.info(f"{user_name} update status for {bot_name} with {now_status}")
             buttons = [
