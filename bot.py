1facd6d4d4f610613f06b18dd2103408edb8ee4f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-06 15:19:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+
+from aiohttp import web
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN
-
-logging.getLogger().setLevel(logging.INFO)
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, PORT
+from plugins import web_server
 
 
 class Bot(Client):
@@ -14,17 +20,21 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=200,
             plugins={"root": "plugins"},
-            sleep_threshold=5,
+            sleep_threshold=10,
         )
         
     async def start(self):
-         await super().start()
-         me = await self.get_me()
-         self.username = me.username
-         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-       
+        await super().start()
+        me = await self.get_me()
+        self.username = me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        await self.send_message(chat_id=LOG_CHANNEL, text=(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
