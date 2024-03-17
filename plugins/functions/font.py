e471f9e78c39175b2a41792eba70482c07c4fd3e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-17 14:17:59 +0530

    Update font.py

--- plugins/functions/font.py
+++ plugins/functions/font.py
@@ -1,22 +1,21 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
+
+# Function to convert text to capital font style
+def convert_to_capital_font(text):
+    capital_font = {
+        'a': 'ᴀ', 'b': 'ʙ', 'c': 'ᴄ', 'd': 'ᴅ', 'e': 'ᴇ', 'f': 'ꜰ', 'g': 'ɢ', 'h': 'ʜ', 'i': 'ɪ', 'j': 'ᴊ',
+        'k': 'ᴋ', 'l': 'ʟ', 'm': 'ᴍ', 'n': 'ɴ', 'o': 'ᴏ', 'p': 'ᴘ', 'q': 'Q', 'r': 'ʀ', 's': 'ꜱ', 't': 'ᴛ',
+        'u': 'ᴜ', 'v': 'ᴠ', 'w': 'ᴡ', 'x': 'x', 'y': 'ʏ', 'z': 'ᴢ'
+    }
+    converted_text = ''.join(capital_font.get(char.lower(), char) for char in text)
+    return converted_text
 
 
-@Client.on_message(filters.command("font") & filters.private)
-async def change_font(client, message):
-    if len(message.command) == 2:
-        text = message.text.split(maxsplit=1)[1]
-        converted_text = ""
-        font_alphabet = {
-            'a': 'ᴀ', 'b': 'ʙ', 'c': 'ᴄ', 'd': 'ᴅ', 'e': 'ᴇ', 'f': 'ꜰ',
-            'g': 'ɢ', 'h': 'ʜ', 'i': 'ɪ', 'j': 'ᴊ', 'k': 'ᴋ', 'l': 'ʟ',
-            'm': 'ᴍ', 'n': 'ɴ', 'o': 'ᴏ', 'p': 'ᴘ', 'q': 'ǫ', 'r': 'ʀ',
-            's': 'ꜱ', 't': 'ᴛ', 'u': 'ᴜ', 'v': 'ᴠ', 'w': 'ᴡ', 'x': 'x',
-            'y': 'ʏ', 'z': 'ᴢ'
-        }
-        for char in text.lower():
-            if char in font_alphabet:
-                converted_text += font_alphabet[char]
-            else:
-                converted_text += char
-        await message.reply(converted_text)
-        
+# Command to handle incoming messages
+@Client.on_message(filters.command("font"))
+def capital_font(client, message):
+    text_to_convert = ' '.join(message.command[1:])
+    converted_text = convert_to_capital_font(text_to_convert)
+    message.reply(converted_text)
+
