ec4c265c2fa4d41c314fa9edf1ec44d3609672bf
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-20 15:10:18 +0530

    Update cinevood.py

--- plugins/download/cinevood.py
+++ plugins/download/cinevood.py
@@ -56,7 +56,7 @@ def search_movies(query):
     if website.status_code == 200:
         website = website.text
         website = BeautifulSoup(website, "html.parser")
-        movies = website.find_all("article", class_="latestPost excerpt")
+        movies = website.find_all("a", class_="latestPost excerpt")
         for movie in movies:
             if movie:
                 movies_details["id"] = f"link{movies.index(movie)}"
