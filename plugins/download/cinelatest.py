d730b616068afc0f9d25062f1645ceaf6b30ccbf
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-21 22:47:30 +0530

    Update cinelatest.py

--- plugins/download/cinelatest.py
+++ plugins/download/cinelatest.py
@@ -15,23 +15,22 @@ async def cine_tranding_movies(client, message):
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for HTTP errors
         soup = BeautifulSoup(response.text, "html.parser")
-        movies = soup.find_all('src')['alt']
-        movie_list = ""
+        movies = [movie['alt'] for movie in soup.find_all('src')]
+        movie_list = "<code>Most Popular Movies:</code>\n\n"
         for movie in movies:
             movie_list += f"<code>{movie}</code>\n\n"
 
         await msg.delete()
-        main = await message.reply_text(f"Most Popular Movies:\n\n{movie_list}", quote=True)
+        main = await message.reply_text(f"{movie_list}", quote=True)
         await client.send_message(
             chat_id=LOG_CHANNEL,
             text=f"Latest Updated Movies:\n\n{movie_list}"
         )
-        
+
         await asyncio.sleep(15)
         await main.delete()
     except Exception as e:
         await message.reply_text(f"An error occurred: {e}")
-
 
 @Client.on_message(filters.command("cinelatest"))
 async def cine_latest_movies(client, message):
