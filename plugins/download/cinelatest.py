ca11f810dfdf8c72b46e3be243307248a450eb3b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-21 19:28:56 +0530

    Create cinelatest.py

---
+++ plugins/download/cinelatest.py
@@ -0,0 +1,66 @@
+# cinelatest.py
+import asyncio
+import requests
+from bs4 import BeautifulSoup
+from pyrogram import Client, filters
+from info import ADMINS, LOG_CHANNEL
+
+
+@Client.on_message(filters.command("cinetranding"))
+async def cine_tranding_movies(client, message):
+    msg = await message.reply_text("Fetching popular movies...", quote=True)
+    domain = await fetch_new_domain()
+    suffix = await fetch_new_suffix()
+    url = f"https://1cinevood.site/"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        soup = BeautifulSoup(response.text, "html.parser")
+        movies = soup.find_all('div', {'class': 's-slider'})
+        movie_list = ""
+        for movie in movies:
+            movie_list += f"<code>{movie}</code>\n\n"  # Remove leading and trailing whitespace
+
+        await msg.delete()
+        main = await message.reply_text(f"Most Popular Movies:\n\n{movie_list}", quote=True)
+        await client.send_message(
+            chat_id=LOG_CHANNEL,
+            text=f"Latest Updated Movies:\n\n{movie_list}"
+        )
+        
+        await asyncio.sleep(15)
+        await main.delete()
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+
+
+@Client.on_message(filters.command("cinelatest"))
+async def cine_latest_movies(client, message):
+    msg = await message.reply_text("Fetching latest movies...", quote=True)
+    domain = await fetch_new_domain()
+    suffix = await fetch_new_suffix()
+    url = f"https://1cinevood.site/"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        soup = BeautifulSoup(response.text, "html.parser")
+        
+        # Extracting only the relevant movie information
+        movies = soup.find_all('div', {'class': 'latestPost excerpt'}) # Start from the fourth Fmvideo div
+        movie_list = ""
+        for movie in movies:
+            movie_list += f"<code>{movie}</code>\n\n"  # Remove leading and trailing whitespace
+        
+        await msg.delete()
+        main = await message.reply_text(f"Latest Updated Movies:\n\n{movie_list}", quote=True)
+        await client.send_message(
+            chat_id=LOG_CHANNEL,
+            text=f"Latest Updated Movies:\n\n{movie_list}"
+        )
+        
+        await asyncio.sleep(15)
+        await main.delete()
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
