1859499b98784206c8cb540ed27636f5f574e123
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-28 18:47:36 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,4 +1,3 @@
-# users_chats_db.py
 import motor.motor_asyncio
 from info import DATABASE_NAME, DATABASE_URI
 
@@ -10,60 +9,26 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
 
-    def new_user(self, id, name):
+    def new_user(self, id, name, referred_by=None):
         return dict(
             id=id,
             name=name,
-            file_id=None,
-            wallet=None,
-            balance=0,
-            referrals=[],
             ban_status=dict(
                 is_banned=False,
                 ban_reason="",
             ),
+            referral=dict(
+                referred_by=referred_by,
+                referral_count=0,
+            ),
+            credits=0,
         )
 
-    async def get_verified_dot(self, user_id, bot_name, now_status):
-        user = await self.col.find_one({'id': int(user_id), 'bot_name': bot_name, 'now_status': now_status})
-        return user.get('verification_status', {}) if user else {}
-
-    async def update_verification_dot(self, user_id, bot_name, now_status, date_temp, time_temp):
-        await self.col.update_one(
-            {'id': int(user_id), 'bot_name': bot_name, 'now_status': now_status},
-            {'$set': {'verification_status': {'date': date_temp, 'time': time_temp}}},
-            upsert=True
-        )
-        
-    async def get_verified_bd(self, user_id, db_name, now_status):
-        user = await self.col.find_one({'id': int(user_id), 'db_name': db_name, 'now_status': now_status})
-        return user.get('verification_status', {}) if user else {}
-
-    async def update_verification_bd(self, user_id, db_name, now_status, date_temp, time_temp):
-        await self.col.update_one(
-            {'id': int(user_id), 'db_name': db_name, 'now_status': now_status},
-            {'$set': {'verification_status': {'date': date_temp, 'time': time_temp}}},
-            upsert=True
-        )
-
-    async def store_invite_link(self, user_id, db_name, channel_id, invite_link, invite_link_count, invite_link_expiry):
-        await self.col.update_one(
-            {'id': int(user_id), 'db_name': db_name, 'channel_id': channel_id},
-            {'$set': {'invite_link_status': {
-                'invite_link': invite_link,
-                'invite_link_count': invite_link_count,
-                'invite_link_expiry': invite_link_expiry
-            }}},
-            upsert=True
-        )
-
-    async def get_store_invite_link(self, user_id, db_name, channel_id):
-        user = await self.col.find_one({'id': int(user_id), 'db_name': db_name, 'channel_id': channel_id})
-        return user.get('invite_link_status', {}) if user else {}
-
-    async def add_user(self, id, name):
-        user = self.new_user(id, name)
+    async def add_user(self, id, name, referred_by=None):
+        user = self.new_user(id, name, referred_by)
         await self.col.insert_one(user)
+        if referred_by:
+            await self.add_referral(referred_by)
 
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
@@ -76,16 +41,12 @@ class Database:
     async def get_user(self, id):
         return await self.col.find_one({'id': int(id)})
 
-    async def get_total_referrals(self, user_id):
-        user = await self.get_user(user_id)
-        return len(user.get('referrals', []))
+    async def get_all_users(self):
+        return self.col.find({})
 
-    async def get_chat_members_count(self, chat_id):
-        chat = await self.get_chat(chat_id)
-        if chat:
-            return chat.get('members', 0)
-        return 0
-        
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
@@ -110,32 +71,45 @@ class Database:
             return default
         return user.get('ban_status', default)
 
-    async def get_all_users(self):
-        return self.col.find({})
-
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
-
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
         b_users = [user['id'] async for user in users]
         return b_users
 
-    async def set_payment_img(self, id, file_id):
-        await self.col.update_one({'id': int(id)}, {'$set': {'file_id': file_id}})
-        
-    async def get_payment_img(self, id):
-        try:
-            image = await self.col.find_one({'id': int(id)})
-            if image:
-                return image.get('file_id')
-            else:
-                return None
-        except Exception as e:
-            print(e)
-            
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
+
+    # New functions for referral and credits management
+
+    async def add_referral(self, id):
+        await self.col.update_one({'id': int(id)}, {'$inc': {'referral.referral_count': 1}})
+
+    async def get_referral(self, id):
+        user = await self.col.find_one({'id': int(id)}, {'referral.referral_count': 1})
+        if not user:
+            return 0
+        return user.get('referral', {}).get('referral_count', 0)
+
+    async def add_credits(self, id, credits):
+        await self.col.update_one({'id': int(id)}, {'$inc': {'credits': credits}})
+
+    async def get_credits(self, id):
+        user = await self.col.find_one({'id': int(id)}, {'credits': 1})
+        if not user:
+            return 0
+        return user.get('credits', 0)
+
+    async def use_credits(self, id, credits):
+        await self.col.update_one({'id': int(id)}, {'$inc': {'credits': -credits}})
+
+    async def get_total_credits(self):
+        pipeline = [
+            {"$group": {"id": None, "totalCredits": {"$sum": "$credits"}}}
+        ]
+        result = await self.col.aggregate(pipeline).to_list(length=None)
+        if result:
+            return result[0].get('totalCredits', 0)
+        return 0
 
 
 db = Database(DATABASE_URI, DATABASE_NAME)
