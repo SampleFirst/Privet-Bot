ab023dd0e6b78c68bc24edb0ea8de19742ba1175
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-30 14:08:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,6 +31,17 @@ async def is_subscribed(bot, query=None, userid=None):
             return True
     return False
 
+def get_size(size):
+    """Get size in readable format"""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+    
 def list_to_str(k):
     if not k:
         return "N/A"
