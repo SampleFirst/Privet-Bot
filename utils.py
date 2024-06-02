e1c4e1394c1f3d50818cd413acb2ac4dab4b937c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-02 18:14:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -212,14 +212,14 @@ async def check_token(bot, userid, token):
     else:
         return False
 
-async def get_token(bot, userid, link, fileid):
+async def get_token(bot, userid, link):
     user = await bot.get_users(userid)
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
     TOKENS[user.id] = {token: False}
-    url = f"{link}verify-{user.id}-{token}-{fileid}"
+    url = f"{link}verify-{user.id}-{token}"
     status = await get_verify_status(user.id)
     date_var = status["date"]
     time_var = status["time"]
