f97247c824d46d2fcf7741d5a8de1d9d2c02f21b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-02 19:53:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, PORT
 from plugins import web_server
-
+from utils import temp
 
 class Bot(Client):
 
@@ -28,6 +28,9 @@ class Bot(Client):
     async def start(self):
         await super().start()
         me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
         self.username = me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         await self.send_message(chat_id=LOG_CHANNEL, text=(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}."))
