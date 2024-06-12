930fbaa67ee031731bcae0479b3df952df829297
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-12 16:07:04 +0530

    Update list.py

--- plugins/list.py
+++ plugins/list.py
@@ -7,11 +7,15 @@ async def add_ott(client, message):
     try:
         ott_name = message.text.split(maxsplit=1)[1]
     except IndexError:
-        await message.reply("Please provide the name of the OTT platform to add.")
+        await message.reply("/add_ott Amazon Prime Video")
         return
 
     try:
-        await db.add_ott(ott_name)
-        await message.reply(f"OTT '{ott_name}' added successfully!")
+        if not await db.is_ott_exist(ott_name):
+            await db.add_ott(ott_name)
+            await message.reply(f"OTT '{ott_name}' added successfully!")
+        else:
+            await message.reply(f"OTT '{ott_name}' already adedd!")
     except Exception as e:
         await message.reply(f"An error occurred while adding the OTT platform: {str(e)}")
+
