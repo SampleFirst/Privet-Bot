6763a68fb5245d4555d42b232a7bad1d27f3f5ca
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-12 15:43:35 +0530

    Update list.py

--- plugins/list.py
+++ plugins/list.py
@@ -4,6 +4,14 @@ from info import ADMINS
 
 @Client.on_message(filters.command("add_ott") & filters.user(ADMINS))
 async def add_ott(client, message):
-    ott_name = message.text.split(maxsplit=1)[1]
-    await db.add_ott(ott_name)
-    await message.reply(f"OTT '{ott_name}' added successfully!")
+    try:
+        ott_name = message.text.split(maxsplit=1)[1]
+    except IndexError:
+        await message.reply("Please provide the name of the OTT platform to add.")
+        return
+
+    try:
+        await db.add_ott(ott_name)
+        await message.reply(f"OTT '{ott_name}' added successfully!")
+    except Exception as e:
+        await message.reply(f"An error occurred while adding the OTT platform: {str(e)}")
