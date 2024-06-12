d15f8ad2b8ce1de19e948501616619de10d88536
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-12 23:21:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -334,32 +334,3 @@ async def settings(client, message):
             reply_to_message_id=message.id
         )
 
-@Client.on_callback_query(filters.regex('toggle_'))
-async def toggle_settings(client, callback_query):
-    setting = callback_query.data.split('_')[1]
-    settings = await db.get_settings()
-    if setting == "refer":
-        settings['refer_on'] = not settings.get('refer_on', False)
-    elif setting == "bonus":
-        settings['daily_bonus'] = not settings.get('daily_bonus', False)
-    elif setting == "withdraw":
-        settings['withdraw_btn'] = not settings.get('withdraw_btn', False)
-        
-    await db.update_settings(settings)
-    
-    buttons = [
-        [
-            InlineKeyboardButton('Refer Earn', callback_data="refer"),
-            InlineKeyboardButton('✅ ON' if settings["refer_on"] else '❌ OFF', callback_data="toggle_refer")
-        ],
-        [
-            InlineKeyboardButton('Daily Bonus', callback_data="bonus"),
-            InlineKeyboardButton('✅ ON' if settings["daily_bonus"] else '❌ OFF', callback_data="toggle_bonus")
-        ],
-        [
-            InlineKeyboardButton('Withdraw BTN', callback_data="withdraw"),
-            InlineKeyboardButton('✅ Always ON' if settings["withdraw_btn"] else '❌ After Earn', callback_data="toggle_withdraw")
-        ],
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await callback_query.message.edit_reply_markup(reply_markup=reply_markup)
