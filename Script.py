0dd3ca21a9a10675197d420f82182b9026c16fa2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-16 00:26:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,19 +1,19 @@
 class script(object):
-    START_TEXT = """Hello {user}!\nWelcome to the Bot.\n
-Watch Ads and Earn. Earn 20 Coins per Ad\n
-Once you reach the threshold, you can buy items from my store.\n
-/help - information on how to earn coins\n
+    START_TEXT = """Hello {user}! Welcome to the Bot.\n
+Watch Ads, Earn Coins, Buy Subscription of Premium Bots\n
+
+/help - information on how to earn coins
 /about - View available items in my store
 /bonus - send Command and get bonus"""
     
     HELP_TEXT = """Hello {user}!\nHere's a step-by-step guide to using the bot:\n
-1) Send the /start command to the bot.
-2) Click the <code>Earn Coins 🪙</code> button or send /earn_coins.
-3) Watch ad and get 20 Coins 
-3) You can watch up to 20 ads per day.
-4) Earn 20 coins for each ad you watch.
-5) Check your total coins by clicking the <code>Balance 💰</code> button or sending the /balance command.
-6) Once you've collected enough coins, check the available items in the store by clicking the <code>My Store 🛒</code> button or sending the /mystore command.
+⭐ Send the /start command to the bot.
+⭐ Click the <code>Earn Coins 🪙</code> button or send /earn_coins.
+⭐ Watch ad and get 20 Coins
+⭐ You can watch up to 20 ads per day.
+⭐ Earn 20 coins for each ad you watch.
+⭐ Check your total coins by clicking the <code>Balance 💰</code> button or sending the /balance command.
+⭐ Once you've collected enough coins, check the available items in the store by clicking the <code>My Store 🛒</code> button or sending the /mystore command.
 
 Note: After watching 20 ads in a day, you will be eligible for a random bonus by clicking the <code>Bonus 🎁</code> button or sending the /bonus command.
 Note: Purchases are only available on Saturdays."""
@@ -24,43 +24,44 @@ Note: Purchases are only available on Saturdays."""
 3) 4GB Rename Bot
 4) 4GB Uploader Bot
 
+Earn 20 Coins per Ad, Once you reach the threshold, you can buy items from my store.
+
 For any queries, ask @xyz."""
 
     FORCESUB_TEXT = """**Hello {user},
 Due to overload only my channel subscribers can use me.\n
-Please join my channel and then start me again!...**"""
+Please join my channels and then start me again!...**"""
 
     BALANCE_TEXT = """🆔 User: {username}\n
 🔗 Refer Earn: {refer}\n
 💳 Coins: {balance}"""
     
-    REFER_TEXT = """Hello {user},
+    REFER_TEXT = """Hello {user},\n
 💰 Per Refer: 10 Coins
 📝 Total Referrals: {total_referrals}\n
 Note: Per Join User Through your referral link, when the user watches their first ad, you earn 10 Coins.\n
-"Share your referral link below:"""
+Share your referral link below:"""
 
-    REFEROFF_TEXT = """Hey {user}! Referral program is currently disabled."""
+    REFEROFF_TEXT = """Hey {user}!\nReferral program is currently disabled."""
     
-    BONUS_TEXT = """Hello {user},\nCongratulation, 🎉 you Received 10 Coins"""
+    BONUS_TEXT = """Hello {user},\n🎉 Congratulation, you Received 10 Bonus Coins"""
     
     BONUSOFF_TEXT = """Hey {user}!\nSorry But Your already Received Bonus of the day 10 Coins"""
     
-    BONUSFLOOD_TEXT = """Hey Admin\nid: {user_id}\nName: {username} Try Again For Bonus"""
+    BONUSFLOOD_TEXT = """Hey Admin\nid: {user_id}\nName: {username}\nTrying Again For Bonus"""
     
     EARNING_TEXT = """"Hey {user} 💕\n\nComplete This Ad And Earn 10 Coins."""
     
-    EARNCOIN_TEXT = """"Congratulations! You've reached the daily coins limit set by our coins management system, Please try again after 24 hours."""
+    EARNCOIN_TEXT = """"Congratulations! You've complete the daily coins limit set by our coins management system, Please try again after 24 hours."""
     
-    WITHDRAW_TEXT = """📤 You can withdraw your balance once you reach the minimum threshold. Please contact support for more details."""
+    MYSTORE_TEXT = """📤 You can withdraw your balance once you reach the minimum threshold. Please contact support for more details."""
     
-    STATS_TEXT = """Bot Stats\n
-Total Users: {total_users}\n"
-Database Size: {size}\n"
-Free Space: {free}"""
+    STATS_TEXT = """**Bot Stats\n
+Total Users: {total_users}
+Database Size: {size}
+Free Space: {free}**"""
             
     LOG_TEXT_P = """#NewUser
 ID - <code>{a}</code>
 Name - {b}
-Mention - {c}
-"""
+Mention - {c}"""
