adaf3467efc6a435b96e094a0a901cf0145e9f68
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-15 22:57:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,12 +1,12 @@
 class script(object):
-    START_TXT = """Hello {user}!\nWelcome to the Bot.\n
+    START_TEXT = """Hello {user}!\nWelcome to the Bot.\n
 Watch Ads and Earn. Earn 20 Coins per Ad\n
 Once you reach the threshold, you can buy items from my store.\n
 /help - information on how to earn coins\n
 /about - View available items in my store
 /bonus - send Command and get bonus"""
     
-    HELP_TXT = """Hello {user}!\nHere's a step-by-step guide to using the bot:\n
+    HELP_TEXT = """Hello {user}!\nHere's a step-by-step guide to using the bot:\n
 1) Send the /start command to the bot.
 2) Click the <code>Earn Coins 🪙</code> button or send /earn_coins.
 3) Watch ad and get 20 Coins 
@@ -18,7 +18,7 @@ Once you reach the threshold, you can buy items from my store.\n
 Note: After watching 20 ads in a day, you will be eligible for a random bonus by clicking the <code>Bonus 🎁</code> button or sending the /bonus command.
 Note: Purchases are only available on Saturdays."""
 
-    ABOUT_TXT = """Hello {user}!\nI am the MyStore Bot. Here, you can buy subscriptions to different bots:\n
+    ABOUT_TEXT = """Hello {user}!\nI am the MyStore Bot. Here, you can buy subscriptions to different bots:\n
 1) DRM Downloader Bot (Limited OTT)
 2) OTT Notification Bot (Limited OTT)
 3) 4GB Rename Bot
@@ -26,6 +26,39 @@ Note: Purchases are only available on Saturdays."""
 
 For any queries, ask @xyz."""
 
+    FORCESUB_TEXT = """**Hello {user},
+Due to overload only my channel subscribers can use me.\n
+Please join my channel and then start me again!...**"""
+
+    BALANCE_TEXT = """🆔 User: {username}\n
+🔗 Refer Earn: {refer}\n
+💳 Coins: {balance}"""
+    
+    REFER_TEXT = """Hello {user},
+💰 Per Refer: 10 Coins
+📝 Total Referrals: {total_referrals}\n
+Note: Per Join User Through your referral link, when the user watches their first ad, you earn 10 Coins.\n
+"Share your referral link below:"""
+
+    REFEROFF_TEXT = """Hey {user}! Referral program is currently disabled."""
+    
+    BONUS_TEXT = """Hello {user},\nCongratulation, 🎉 you Received 10 Coins"""
+    
+    BONUSOFF_TEXT = """Hey {user}!\nSorry But Your already Received Bonus of the day 10 Coins"""
+    
+    BONUSFLOOD_TEXT = """Hey Admin\nid: {user_id}\nName: {username} Try Again For Bonus"""
+    
+    EARNING_TEXT = """"Hey {user} 💕\n\nComplete This Ad And Earn 10 Coins."""
+    
+    EARNCOIN_TEXT = """"Congratulations! You've reached the daily coins limit set by our coins management system, Please try again after 24 hours."""
+    
+    WITHDRAW_TEXT = """📤 You can withdraw your balance once you reach the minimum threshold. Please contact support for more details."""
+    
+    STATS_TEXT = """Bot Stats\n
+Total Users: {total_users}\n"
+Database Size: {size}\n"
+Free Space: {free}"""
+            
     LOG_TEXT_P = """#NewUser
 ID - <code>{a}</code>
 Name - {b}
