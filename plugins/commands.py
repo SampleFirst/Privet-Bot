d3c8e102ec5c24bae2c3901814573f2e7a6dc27e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-15 22:54:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,12 +24,12 @@ async def get_buttons(user_id):
     
     bonus = await db.get_bonus_status(user_id)
     if bonus["got_bonus"]:
-        buttons.append(["Earn Credits 💵"])
+        buttons.append(["Earn Coins 💵"])
     else:
         buttons.append(["Bonus 🎁"])
     
-    user_credits = await db.get_credits(user_id)
-    if settings["withdraw_btn"] or user_credits >= 100:
+    user_coins = await db.get_coins(user_id)
+    if settings["withdraw_btn"] or user_coins >= 100:
         buttons[-1].append("📤 Withdraw")
 
     # Flatten the buttons list
@@ -60,7 +60,7 @@ async def start(client, message):
     if len(message.command) != 2:
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(user=message.from_user.mention),
+            caption=script.START_TEXT.format(user=message.from_user.mention),
             reply_markup=buttonz,
             parse_mode=enums.ParseMode.HTML,
             quote=True
@@ -77,7 +77,7 @@ async def start(client, message):
         btn = [[InlineKeyboardButton("Join Our Back-Up Channel", url=invite_link.invite_link)]]
         await client.send_message(
             chat_id=message.from_user.id,
-            text=f"**Hello {message.from_user.mention}, Due to overload only my channel subscribers can use me.\n\nPlease join my channel and then start me again!...**",
+            text=script.FORCESUB_TEXT.format(user=message.from_user.mention),
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN,
             quote=True
@@ -88,7 +88,7 @@ async def start(client, message):
     if data in ["subscribe", "error", "okay", "help"]:
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(user=message.from_user.mention),
+            caption=script.START_TEXT.format(user=message.from_user.mention),
             reply_markup=buttonz,
             parse_mode=enums.ParseMode.HTML,
             quote=True
@@ -103,7 +103,7 @@ async def start(client, message):
 
         is_valid = await check_token(client, userid, token)
         if is_valid:
-            await db.add_credits(userid, 20)
+            await db.add_coins(userid, 20)
             await message.reply_text(text="Hey user You are successful verification")
         else:
             await message.reply_text(text="<b>Invalid or Expired Link!</b>")
@@ -123,30 +123,39 @@ async def start(client, message):
 async def balance(bot, message):
     user_id = message.from_user.id
     username = message.from_user.username or "N/A"
-    balance = await db.get_credits(user_id)
+    balance = await db.get_coins(user_id)
     refer = await db.get_referral(user_id)
-    await message.reply(
-        f"🆔 User: {username}\n\n🔗 Refer Earn: {refer}\n💳 Credits: {balance}",
+    await message.reply_text(
+        text=script.BALANCE_TEXT.format(username, refer, balance),
         quote=True
     )
-    
+
 @Client.on_message(filters.regex('🗣 Referral') & filters.private)
 async def referral(bot, message):
     settings = await db.get_settings()
     user_id = message.from_user.id
+    user = message.from_user.first_name
     
     if settings['refer_on']:
         total_referrals = await db.get_referral(user_id)
         referral_link = f"https://telegram.me/{temp.U_NAME}?start=refer-{user_id}"
-        await message.reply(
-            f"💰 Per Refer: Upto 50 Coins\n\n📝 Total Referrals: {total_referrals}\n\n🔍 Your Referral Link: {referral_link}",
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Share Your Link", url=f"https://t.me/share/url?url={referral_link}")
+                ]
+            ]
+        )
+        await message.reply_text(
+            text=script.REFER_TEXT.format(user, total_referrals),
+            reply_markup=reply_markup,
             disable_web_page_preview=True,
             quote=True
         )
     else:
         buttonz = await get_buttons(message.from_user.id)
-        msg = await message.reply(
-            text="Referral program is currently disabled.",
+        msg = await message.reply_text(
+            text=script.REFEROFF_TEXT.format(user),
             reply_markup=buttonz,
             quote=True
         )
@@ -157,50 +166,61 @@ async def referral(bot, message):
 @Client.on_message(filters.regex('Bonus 🎁') & filters.private)
 async def bonus(bot, message):
     user_id = message.from_user.id
+    user = message.from_user.first_name
     username = message.from_user.username or "N/A"
     bonus = await db.get_bonus_status(user_id)
     if bonus["got_bonus"] == True:
         buttonz = await get_buttons(user_id)
-        await message.reply(
-            "🎁 Your already Received 20 Credits", 
+        await message.reply_text(
+            text=script.BONUSOFF_TEXT.format(user),
             reply_markup=buttonz,
             quote=True
         )
-        await bot.send_message(LOG_CHANNEL, f"Hey Admin {user_id} Name: {username} Try Again For Bonus")
+        await bot.send_message(LOG_CHANNEL, script.BONUSFLOOD_TEXT.format(user_id, username)
     else:
         await db.got_bonus_status(user_id)
-        await db.add_credits(user_id, 20)
+        await db.add_coins(user_id, 10)
         buttonz = await get_buttons(user_id)
-        await message.reply(
-            "🎁 Congratulation, you Received 20 Credits", 
+        await message.reply_text(
+            text=script.BONUS_TEXT.format(user),
             reply_markup=buttonz,
             quote=True
         )
 
-@Client.on_message(filters.regex('Earn Credits 💵') & filters.private)
-async def earn_credits(client, message):
-    if not await check_verification(client, message.from_user.id):
-        btn = [[
-            InlineKeyboardButton(f"Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=")),
-            InlineKeyboardButton("How To Verify", url="https://t.me/+IvrcMfPKCMxkNjVl")
-        ]]
-        await client.send_message(
-            chat_id=message.from_user.id,
-            text=f"Hey {message.from_user.mention} 💕\n\nComplete This Ad And Earn 20 Credits.",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(btn)
-        )
-        return
-    else:
-        await client.send_message(
-            chat_id=message.from_user.id,
-            text="Congratulations! You've reached the daily credits limit set by our credits management system, Please try again after 24 hours."
-        )
-
+@Client.on_message(filters.regex('Earn Coins 💵') & filters.private)
+async def earn_coins(client, message):
+    user = message.from_user.mention
+    try:
+        if not await check_verification(client, message.from_user.id):
+            btn = [[
+                InlineKeyboardButton(f"Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=")),
+                InlineKeyboardButton("How To Verify", url="https://t.me/+IvrcMfPKCMxkNjVl")
+            ]]
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text=script.EARNCOIN_TEXT.format(user),
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(btn)
+            )
+            return
+        else:
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text=script.EARNCOINS_TEXT.format(user)
+            )
+    except Exception as e:
+        await message.reply(str(e))
+        
 @Client.on_message(filters.regex('📤 Withdraw') & filters.private)
 async def withdraw(bot, message):
-    await message.reply("📤 You can withdraw your balance once you reach the minimum threshold. Please contact support for more details.", quote=True)
-
+    try:
+        await message.reply_text(
+            text=script.WITHDRAW_TEXT.format()
+            quote=True
+        )
+    except Exception as e:
+        await message.reply(str(e))
+        
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
     try:
@@ -214,7 +234,7 @@ async def help(client, message):
     try:
         user = message.from_user.first_name
         await message.reply_text(
-            text=script.HELP_TXT.format(user=user),
+            text=script.HELP_TEXT.format(user=user),
             quote=True
         )
     except Exception as e:
@@ -225,9 +245,8 @@ async def help(client, message):
 async def about(client, message):
     try:
         user = message.from_user.first_name
-        about_text = script.ABOUT_TXT.format(user=user)
         await message.reply_text(
-            text=script.ABOUT_TXT.format(user=user),
+            text=script.ABOUT_TEXT.format(user=user),
             quote=True
         )
     except Exception as e:
@@ -235,7 +254,7 @@ async def about(client, message):
     
 @Client.on_message(filters.command('stats') & filters.incoming)
 async def get_stats(bot, message):
-    rju = await message.reply('Fetching stats..')
+    msg = await message.reply('Fetching stats..')
     try:
         total_users = await db.total_users_count()
         size = await db.get_db_size()
@@ -243,33 +262,30 @@ async def get_stats(bot, message):
         size = get_size(size)
         free = get_size(free)
         
-        stats_message = (
-            f"**Bot Stats**\n\n"
-            f"**Total Users:** {total_users}\n"
-            f"**Database Size:** {size}\n"
-            f"**Free Space:** {free}"
-        )
-        await rju.edit(stats_message)
+        await msg.edit(script.STATS_TEXT.format(total_users, size, free)
     except Exception as e:
-        await rju.edit(f"An error occurred: {e}")
+        await msg.edit(f"An error occurred: {e}")
 
 @Client.on_message(filters.command('users') & filters.user(ADMINS))
 async def list_users(bot, message):
-    raju = await message.reply('Getting List Of Users')
-    users = await db.get_all_users()
-    out = "Users Saved In DB Are:\n\n"
-    async for user in users:
-        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
-        if user['ban_status']['is_banned']:
-            out += '( Banned User )'
-        out += '\n'
     try:
-        await raju.edit_text(out)
-    except MessageTooLong:
-        with open('users.txt', 'w+') as outfile:
-            outfile.write(out)
-        await message.reply_document('users.txt', caption="List Of Users")
-
+        msg = await message.reply('Getting List Of Users')
+        users = await db.get_all_users()
+        out = "Users Saved In DB Are:\n\n"
+        async for user in users:
+            out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
+            if user['ban_status']['is_banned']:
+                out += '( Banned User )'
+            out += '\n'
+        try:
+            await msg.edit_text(out)
+        except MessageTooLong:
+            with open('users.txt', 'w+') as outfile:
+                outfile.write(out)
+            await message.reply_document('users.txt', caption="List Of Users")
+    except Exception as e:
+        await msg.edit(f"An error occurred: {e}")
+        
 @Client.on_message(filters.command('deleteusers') & filters.user(ADMINS))
 async def deleteusers(bot, message):
     msg = await message.reply('Starting deletion of users...')
