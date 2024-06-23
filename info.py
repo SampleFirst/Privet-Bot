66cc4d254d9d2efcf071ebd72098a1221e2c7042
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-23 20:10:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,29 +48,29 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/c/1845700490/3")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', "runurl.in") # runurlx
 SHORTLINK_API = environ.get('SHORTLINK_API', "868966540ff18e4c2ad2e73343cb2f33181acedb")
 
-VERIFY2_URL = environ.get('VERIFY2_URL', "runurl.in") # Frist 
+VERIFY2_URL = environ.get('VERIFY2_URL', "runurl.in") # Frist 12$ 960r
 VERIFY2_API = environ.get('VERIFY2_API', "5ed6df09ca6970fb5809dd7b4388b19bc12e9bca")
 
-VERIFY3_URL = environ.get('VERIFY3_URL', "runurl.in") # Third 
+VERIFY3_URL = environ.get('VERIFY3_URL', "runurl.in") # Third 12$ 960r
 VERIFY3_API = environ.get('VERIFY3_API', "4333efb61c180fb738724bfbe54427ba1b666169")
 
-VERIFY4_URL = environ.get('VERIFY4_URL', "runurl.in") # Fourth
+VERIFY4_URL = environ.get('VERIFY4_URL', "runurl.in") # Fourth 12$ 960r
 VERIFY4_API = environ.get('VERIFY4_API', "d45daa443ecff47b5a4ebae3c803ee3da53a96f4")
 
-VERIFY5_URL = environ.get('VERIFY5_URL', "kingurl.in") # Ongoing 
+VERIFY5_URL = environ.get('VERIFY5_URL', "kingurl.in") # Ongoing 10$ 800r
 VERIFY5_API = environ.get('VERIFY5_API', "d5be075758edd71808de5e98b339af720e5eabce")
 
-VERIFY6_URL = environ.get('VERIFY6_URL', "kingurl.in")
-VERIFY6_API = environ.get('VERIFY6_API', "c2150e28189cefefd05f8a9c5c5770cc462033e3")
+VERIFY6_URL = environ.get('VERIFY6_URL', "kingurl.in") # Frists 10$ 800r
+VERIFY6_API = environ.get('VERIFY6_API', "ff103ac2249036f5aa5eecd61a734c946bc5019a")
 
-VERIFY7_URL = environ.get('VERIFY7_URL', "vipurl.in") # Frist 
+VERIFY7_URL = environ.get('VERIFY7_URL', "vipurl.in") # Frist 7$ 539r or 560r
 VERIFY7_API = environ.get('VERIFY7_API', "ba4c12b96125e2cc04402e2a9125a8559f27b3fc")
 
-VERIFY8_URL = environ.get('VERIFY8_URL', "vipurl.in") # Second 
+VERIFY8_URL = environ.get('VERIFY8_URL', "vipurl.in") # Second 7$ 539r or 560r
 VERIFY8_API = environ.get('VERIFY8_API', "dab3cb1c6b806d8ac96c0635793f12ad87cce803")
 
-VERIFY9_URL = environ.get('VERIFY9_URL', "ziplinker.net") # oneman
+VERIFY9_URL = environ.get('VERIFY9_URL', "ziplinker.net") # oneman 450r
 VERIFY9_API = environ.get('VERIFY9_API', "603c7a037b8bf4d5a9d2eaa43df6b84783da7aa6")
 
-VERIFY10_URL = environ.get('VERIFY10_URL', "ziplinker.net") # adventure 
+VERIFY10_URL = environ.get('VERIFY10_URL', "ziplinker.net") # adventure 450r
 VERIFY10_API = environ.get('VERIFY10_API', "12f1784cd5831936c8e4c6f5e414a2f31a22eb2f")
