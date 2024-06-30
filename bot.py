e38e87b81e6f2dc9202c5234d96b02a6068d03ae
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-30 18:48:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 
 import os
+from datetime import date, datetime 
+import pytz
 from aiohttp import web
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
@@ -32,8 +34,14 @@ class Bot(Client):
         temp.ME = me.id
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
-        self.username = me.username
-        await self.send_message(chat_id=LOG_CHANNEL, text=(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}."))
+        self.id = me.id
+        self.mention = me.mention
+        self.uptime = UPTIME
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(a=today, b=time, c=temp.U_NAME))
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
@@ -41,7 +49,8 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("Bot Restarting.......")
+        me = await self.get_me()
+        logger.info(f"{me.first_name} is restarting...")
 
 app = Bot()
 app.run()
