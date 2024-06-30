2a960da10137482f71de0be4a6ad64410e34e435
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-30 19:50:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,38 @@
-import logging
-import logging.config
-import asyncio
-
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-
 import os
-from Script import script
+import math
+import logging
 from datetime import date, datetime 
 import pytz
+import asyncio
+import pytz
+from typing import Union, Optional, AsyncGenerator
+
 from aiohttp import web
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, UPTIME, LOG_CHANNEL, PORT
-from plugins import web_server
+from pyrogram import Client, types
+from pyrogram.errors import BadRequest, Unauthorized
+
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, PORT
 from utils import temp
+from plugins import web_server
+from Script import script 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            name="Professor-Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=200,
             plugins={"root": "plugins"},
-            sleep_threshold=5,
+            sleep_threshold=10,
         )
-        
+
     async def start(self):
         await super().start()
         me = await self.get_me()
@@ -52,6 +56,17 @@ class Bot(Client):
         await super().stop()
         me = await self.get_me()
         logger.info(f"{me.first_name} is restarting...")
+
+    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
 app = Bot()
 app.run()
