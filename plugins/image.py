a17ab512a1f6cec9c5e66b700f6d0d51c15bce52
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-07-07 23:47:16 +0530

    Update image.py

--- plugins/image.py
+++ plugins/image.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from PIL import Image, ImageDraw, ImageFont
-import Arial.ttf
 
 # Function to create an image with text
 def create_image_with_text(text):
     img = Image.new('RGB', (500, 300), color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("arial.ttf", 20)
+    font_path = "arial.ttf"
+    font = ImageFont.truetype(font_path, 20)
     text_width, text_height = draw.textsize(text, font=font)
     position = ((img.width - text_width) // 2, (img.height - text_height) // 2)
     draw.text(position, text, fill=(0, 0, 0), font=font)
@@ -19,3 +19,4 @@ def generate_image(client, message):
     text = message.text.split(" ", 1)[1]
     image_path = create_image_with_text(text)
     client.send_photo(chat_id=message.chat.id, photo=image_path)
+
