dc5097909224d283ee1274beb36dd3dc0398b6a1
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-12-08 19:41:24 +0530

    Update cinevood.py

--- plugins/download/cinevood.py
+++ plugins/download/cinevood.py
@@ -46,7 +46,7 @@ async def movie_result(client, callback_query):
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    website = requests.get(f"https://1cinevood.site/?s={query.replace(' ', '+')}")
+    website = requests.get(f"https://1cinevood.digital/?s={query.replace(' ', '+')}")
     if website.status_code == 200:
         website = website.text
         website = BeautifulSoup(website, "html.parser")
