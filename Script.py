2cceaae617a07c2bcc2f178ffcc28120cfbbd53e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-12-15 16:41:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,9 +1,8 @@
 class script(object):
-    START_TXT = """Hello {user}!\nWelcome to the Bot."""
+    START_TXT = """Hello {user}!\nWelcome to the Bot.\nuse command for serch movies\n\nExample /cinevood Pushpa 2"""
 
     LOG_TEXT_P = """#NewUser
 ID - <code>{a}</code>
 Name - {b}
 Mention - @{c}
 """
-    
