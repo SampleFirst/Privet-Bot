115105f97bda24703c10396a059ea0fec3ea43ba
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-12-16 21:27:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,8 +46,8 @@ MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', 0))
 
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/c/1845700490/3")
-VERIFY2_URL = environ.get('VERIFY2_URL', "clicksfly.com")
-VERIFY2_API = environ.get('VERIFY2_API', "c2150e28189cefefd05f8a9c5c5770cc462033e3")
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'clicksfly.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'c2150e28189cefefd05f8a9c5c5770cc462033e3')
+VERIFY2_URL = environ.get('VERIFY2_URL', "adlinkfly.in")
+VERIFY2_API = environ.get('VERIFY2_API', "d0838b901b0d60cfa9dd2a00ddb6f1251addc52f")
+SHORTLINK_URL = environ.get('SHORTLINK_URL', "runurl.in")
+SHORTLINK_API = environ.get('SHORTLINK_API', "868966540ff18e4c2ad2e73343cb2f33181acedb")
 
